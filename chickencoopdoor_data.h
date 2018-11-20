04fc4b7f45d6bcad2a7fa89428c5527355d25e1e
Gordlonious <gordlonious@gmail.com>
2018-11-19 19:17:34 -0700

    motor control logic added, consistency concept reverted

--- chickencoopdoor_data.h
+++ chickencoopdoor_data.h
@@ -1,18 +1,14 @@
 const int StepperPin1 = A2;
 const int StepperPin2 = A4;
-const int EncoderPin1 = 6;
-const int EncoderPin2 = 8;
+const int MotorControlPin1 = 6;
+const int MotorControlPin2 = 8;
 
 const int DaylightPin = A6; // defaults to INPUT mode
 
 struct LightData  {
 	int lightLevel;
-	bool isConsistent;
+	bool isConsistent;  // not used but could be useful in real-world scenario
 };
 
-// classify resistance value ranges for photo sensor
-// based on return value of analogRead()
-const int NightMin = 0;
-const int NightMax = 300;
-const int DayMin = 600;
-const int DayMax = 1023;
+// sensor provides good light/dark outputs, use midpoint to detect state
+const int LightMidpoint = 50;
--- sketch_chickencoopdoor.ino
+++ sketch_chickencoopdoor.ino
@@ -3,66 +3,58 @@
   #include "chickencoopdoor_data.h"
 #endif
 
-bool DoorIsOpen = false; // assume door is shut to start with (night)
+LightData lightState;
 
 void setup() {
   pinMode(StepperPin1, OUTPUT); // each output pin can provide ~40mA of current to external hardware
   pinMode(StepperPin2, OUTPUT);
+  lightState.lightLevel = 0; // assume dark
 }
+
+ unsigned int AnalogValue;
 
 void loop() 
 {
-  LightData ld = CheckForConsistentLightReading(3);
-  if(ld.isConsistent) 
-  {
-    // change state of door if it has changed
-    if(ld.lightLevel == 3 && !DoorIsOpen || ld.lightLevel == 1 && DoorIsOpen)
-    {
-      digitalWrite(StepperPin1, HIGH);
-      digitalWrite(StepperPin2, HIGH);
-
-      delay(3000); // how long does it take to change the door state? 
-      // TODO: Change voltage levels in a particular way to signal/differentiate between 'open' and 'close' motor operations (use motor control pins)
-
-      digitalWrite(StepperPin1, LOW);
-      digitalWrite(StepperPin2, LOW);
+  unsigned int l = readLight();
+  if(l != -1) {
+    // dark to light
+    if(lightState.lightLevel == 0 && lightState.lightLevel != l) {
+      TurnCounterClockwise();
+      lightState.lightLevel = l;
+      delay(3000);
+      StopTurn();
+    }
+    // light to dark
+      if(lightState.lightLevel == 1 && lightState.lightLevel != l) {
+      TurnClockwise();
+      lightState.lightLevel = l;
+      delay(3000);
+      StopTurn();
     }
   }
 }
 
-// param 1: duration (in seconds) that the light level is consistently 'light' or 'dark' before returning
-// returns: struct indicating whether sensor reads have been consistent and whether the consistent reads were 'light' or 'dark'
-// TODO: decrease amount of reads (include delay, effects duraction math)
-LightData CheckForConsistentLightReading(int dur) {
-  int nor = 1000000 * dur / 100;
-  LightData ld;
-  int initialRead = readLight();
-  for(int i = 0; i < nor; i++) {
-    int d = readLight();
-    if(d != initialRead) {
-      ld.isConsistent = false;
-      return ld;
-    }
-    if(i+1 == nor) {
-      ld.lightLevel = d;
-      ld.isConsistent = true;
-      return ld;
-    }
-  }
-  ld.isConsistent = false;
-  return ld;
+void TurnClockwise() {
+  digitalWrite(StepperPin1, HIGH);
+  digitalWrite(StepperPin2, LOW);
 }
 
-// returns 1 if analog read from light pin was in night range
-// returns 2 if read was in nuetral range
-// returns 3 if read was in day range
+void TurnCounterClockwise() {
+  digitalWrite(StepperPin1, LOW);
+  digitalWrite(StepperPin2, HIGH);  
+}
+
+void StopTurn() {
+  digitalWrite(StepperPin1, LOW);
+  digitalWrite(StepperPin2, LOW);  
+}
+
 int readLight() {
   int d = analogRead(DaylightPin); // 1 read takes ~100 microseconds
-  if(d >= NightMin && d <= NightMax) {
-    return 1;
-  } else if(d > NightMax && d < DayMin) {
-    return 2;
-  } else if(d >= DayMin && d <= DayMax) {
-    return 3;
-  } else { return 0; }
+  if(d > LightMidpoint) {
+    return 1; // light;
+  } else if(d < LightMidpoint) {
+    return 0; // no light
+  } else { return -1; }
 }
+
